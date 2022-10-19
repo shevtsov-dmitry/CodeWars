539e8df80fa0576131042bd9b901c6bef9c270c8
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2022-10-19 12:13:48 +0300

    I added comment

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -3,6 +3,7 @@ public class CodeWars {
     public static void main(String[] Args) {
         System.out.println(ValidParentheses.validParentheses("(())((()())())"));
         System.out.println(ValidParentheses.validParentheses(")(()))"));
-}}
+        //am I dreaming?
+    }}
 
 
