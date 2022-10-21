0605cd92a06c20ce5178b1aac96283d697397992
Shado <94776476+shevtsov-dmitry@users.noreply.github.com>
2022-10-21 17:28:45 +0300

    did a few exercises

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 
 public class CodeWars {
     public static void main(String[] Args) {
-        System.out.println(ValidParentheses.validParentheses("(())((()())())"));
-        System.out.println(ValidParentheses.validParentheses(")(()))"));
-        //am I dreaming?
-    }}
+
+    }
+
+}
 
 
